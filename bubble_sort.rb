988a36876484f590b9dcb9ecebbcbb9a0d6187e1
r4vsdev <r4vsdev@gmail.com>
2023-02-16 18:51:15 -0300

    Add minor corrections

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 def bubble_sort(array)
   (array.size - 1).times do
-    for i in 0..array.size - 2
-      if array[i] > array[i+1] then
-        aux = array[i]
-        array[i] = array[i+1]
-        array[i+1] = aux
-      end
+    (0..array.size - 2).each do |i|
+      next unless array[i] > array[i + 1]
+
+      aux = array[i]
+      array[i] = array[i + 1]
+      array[i + 1] = aux
     end
   end
   array
 end
 
-array = [4,3,78,2,0,2]
-p bubble_sort(array) # => [0,2,2,3,4,78]
+array = [4, 3, 78, 2, 0, 2]
+puts bubble_sort(array) # => [0,2,2,3,4,78]
